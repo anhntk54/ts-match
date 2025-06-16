e9cf0faa3addcdeae4fdf39e099cffe556f04487
anhnhu <anhnt@konistudio.xyz>
2025-06-16 19:57:11 +0700

    feat: test dev

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // src/index.ts
 export function add(a: number, b: number): number {
-  return a + b;// This function adds two numbers
+  return a + b + b;// This function adds two numbers
 }
 
 export function subtract(a: number, b: number): number {
