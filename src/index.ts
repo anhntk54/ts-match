253bbfd3e9f604239ba9e3da65b6b89fc013a8d2
anhnhu <anhnt@konistudio.xyz>
2025-06-17 09:59:50 +0700

    fix:  minor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // src/index.ts
 export function add(a: number, b: number): number {
-  return a + b + 1;// This function adds two numbers
+  return a + b + 12;// This function adds two numbers
 }
 
 export function subtract(a: number, b: number): number {
