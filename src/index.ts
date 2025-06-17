d4b25c04cd8819a85282e4156dd40e22ed7c309c
anhnhu <anhnt@konistudio.xyz>
2025-06-17 10:53:15 +0700

    feat:  1231

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // src/index.ts
 export function add(a: number, b: number): number {
-  return a + b + 111221;// This function adds two numbers
+  return a + b + 1112221;// This function adds two numbers
 }
 
 export function subtract(a: number, b: number): number {
